8bdc3c1cd82cdade59862fe233590d6beeb56547
justforlxz <justforlxz@gmail.com>
2021-09-16 17:47:33 +0800

    update

--- install.sh
+++ install.sh
@@ -1,17 +1,7 @@
 echo "Start installing..."
 
-tar -xvf deepin-wine.tar
+unzip deepin-wine.zip -d deepin-wine
 
-for _deb in $(ls *.deb); do
-    echo "Installing $_deb"
-    ar p "$_deb" data.tar.xz | tar -xJ
-done
-
-rm -rf /usr/share/doc
-rm -rf /usr/share/man
-
-cp -a usr /
-cp -a opt /
-cp *.ttc /usr/share/deepin-wine5/wine/fonts
+cp -r deepin-wine/* /
 
 echo "Done"
