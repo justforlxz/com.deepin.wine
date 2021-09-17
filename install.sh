b4789f00a9faa0835fe1cfac7de62cf7ae129f61
justforlxz <justforlxz@gmail.com>
2021-09-17 18:09:11 +0800

    update

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 echo "Start installing..."
 
-tar -xvJf deepin-wine.tar.xz
+for _deb in $(ls *.deb); do
+    echo "Installing $_deb"
+    ar p "$_deb" data.tar.xz | tar -xJ
+done
+
+patch -p0 < patch.0
 
 rm -rf /usr/share/{man,doc}
 
-mkdir /usr/bin
-mkdir /usr/lib
-mkdir /usr/share/
-
 cp -r opt/deepin-wine5-stable /opt/
 cp -r usr/bin/* /usr/bin/
-cp -r usr/lib/deepin-wine5 /usr/lib/
 cp -r usr/lib/p7zip /usr/lib/
-cp -r usr/i386-linux-gnu/deepin-wine5 /usr/i386-linux-gnu/
+cp -r usr/include /usr/
+cp -r usr/lib/deepin-wine5 /usr/lib/
+cp -r usr/lib/libudis* /usr/lib/
+cp -r usr/lib/i386-linux-gnu/deepin-wine5 /usr/lib/i386-linux-gnu/
 cp -r usr/share/* /usr/share/
+cp *.ttc /usr/share/deepin-wine5/wine/fonts
 
 echo "Done"
