439021e949d260b48fc56e603c1b6ddeec716dcf
justforlxz <justforlxz@gmail.com>
2021-09-17 20:25:08 +0800

    update

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ patch -p0 < patch.0
 
 rm -rf /usr/share/{man,doc}
 
-cp -r opt/deepin-wine5-stable /opt/
+mkdir /opt
+cp -r opt/* /opt/
 cp -r usr/bin/* /usr/bin/
 cp -r usr/lib/p7zip /usr/lib/
 cp -r usr/include /usr/
-cp -r usr/lib/deepin-wine5 /usr/lib/
 cp -r usr/lib/libudis* /usr/lib/
-cp -r usr/lib/i386-linux-gnu/deepin-wine5 /usr/lib/i386-linux-gnu/
+cp -r usr/lib/i386-linux-gnu/deepin-wine /usr/lib/i386-linux-gnu/
 cp -r usr/share/* /usr/share/
 cp *.ttc /usr/share/deepin-wine5/wine/fonts
 
