d0926faa395a15bc5fc4915eb5b4b21993c2810e
justforlxz <justforlxz@gmail.com>
2021-09-17 13:19:10 +0800

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 echo "Start installing..."
 
-unzip deepin-wine.zip -d deepin-wine
+tar -xvJf deepin-wine.tar.xz
 
-cp -r deepin-wine/* /
+rm -rf /usr/share/{man,doc}
+
+mkdir /usr/bin
+mkdir /usr/lib
+mkdir /usr/share/
+
+cp -r opt/deepin-wine5-stable /opt/
+cp -r usr/bin/* /usr/bin/
+cp -r usr/lib/deepin-wine5 /usr/lib/
+cp -r usr/lib/p7zip /usr/lib/
+cp -r usr/i386-linux-gnu/deepin-wine5 /usr/i386-linux-gnu/
+cp -r usr/share/* /usr/share/
 
 echo "Done"
