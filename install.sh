735ee588e080a937e91c2aaf48f5e8403cdb9c2c
justforlxz <justforlxz@gmail.com>
2021-09-18 13:49:40 +0800

    update

--- install.sh
+++ install.sh
@@ -6,22 +6,6 @@ for _deb in $(ls *.deb); do
 done
 
 patch -p0 < patch.0
-
-cp -dr usr/* $FLATPAK_DEST/
-mkdir $FLATPAK_DEST/opt
-cp -dr opt/* $FLATPAK_DEST/opt/
-
-
-#rm -rf /usr/share/{man,doc}
-
-#mkdir /opt
-#cp -r opt/* /opt/
-#cp -r usr/bin/* /usr/bin/
-#cp -r usr/lib/p7zip /usr/lib/
-#cp -r usr/include /usr/
-#cp -r usr/lib/libudis* /usr/lib/
-#cp -r usr/lib/i386-linux-gnu/deepin-wine /usr/lib/i386-linux-gnu/
-#cp -r usr/share/* /usr/share/
-#cp *.ttc /usr/share/deepin-wine5/wine/fonts
+sed -i "s/\/opt/\/usr/g" `grep /opt -rl opt`
 
 echo "Done"
