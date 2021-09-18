cda6b33dd4eac9505260dd6307f0d14975421e2c
justforlxz <justforlxz@gmail.com>
2021-09-18 10:39:27 +0800

    test

--- install.sh
+++ install.sh
@@ -7,16 +7,21 @@ done
 
 patch -p0 < patch.0
 
-rm -rf /usr/share/{man,doc}
+cp -dr usr/* $FLATPAK_DEST/
+mkdir $FLATPAK_DEST/opt
+cp -dr opt/* $FLATPAK_DEST/opt/
 
-mkdir /opt
-cp -r opt/* /opt/
-cp -r usr/bin/* /usr/bin/
-cp -r usr/lib/p7zip /usr/lib/
-cp -r usr/include /usr/
-cp -r usr/lib/libudis* /usr/lib/
-cp -r usr/lib/i386-linux-gnu/deepin-wine /usr/lib/i386-linux-gnu/
-cp -r usr/share/* /usr/share/
-cp *.ttc /usr/share/deepin-wine5/wine/fonts
+
+#rm -rf /usr/share/{man,doc}
+
+#mkdir /opt
+#cp -r opt/* /opt/
+#cp -r usr/bin/* /usr/bin/
+#cp -r usr/lib/p7zip /usr/lib/
+#cp -r usr/include /usr/
+#cp -r usr/lib/libudis* /usr/lib/
+#cp -r usr/lib/i386-linux-gnu/deepin-wine /usr/lib/i386-linux-gnu/
+#cp -r usr/share/* /usr/share/
+#cp *.ttc /usr/share/deepin-wine5/wine/fonts
 
 echo "Done"
